4fb8ab68b4892ccad2112936058246d2bd3b95ba
Dz.dev <darwinbjordan@gmail.com>
2024-08-02 22:37:06 +0800

    progress: added dummy data to test if working and type for typescript

--- routes/liabilities.ts
+++ routes/liabilities.ts
@@ -1,9 +1,45 @@
 import { Hono } from "hono";
 
+type Liabilities = {
+  id: number;
+  title: string;
+  amount: number;
+};
+
+const dummyData: Liabilities[] = [
+  {
+    id: 1,
+    title: "Food",
+    amount: 200,
+  },
+  {
+    id: 2,
+    title: "Commute",
+    amount: 100,
+  },
+  {
+    id: 3,
+    title: "Internet",
+    amount: 800,
+  },
+  {
+    id: 4,
+    title: "Medical",
+    amount: 500,
+  },
+  {
+    id: 5,
+    title: "Etc",
+    amount: 1000,
+  },
+];
+
 export const liabilitiesRoute = new Hono()
   .get("/", (c) => {
-    return c.json({ liabilities: [] });
+    return c.json({ liabilities: dummyData });
   })
-  .post("/", (c) => {
-    return c.json({});
+  .post("/", async (c) => {
+    const data = await c.req.json();
+    console.log(data);
+    return c.json(data);
   });
