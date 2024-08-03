7f9abe7463a7b14f401cb32bc5acbdd7fd1b16c2
Dz.dev <darwinbjordan@gmail.com>
2024-08-03 17:05:25 +0800

    progress: added useEffect to fetch data from backend and reflect it on frontend

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,10 +5,19 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function App() {
   const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    async function fetchDrained() {
+      const res = await fetch("/api/liabilities/total-drained");
+      const data = await res.json();
+      setCount(data.total);
+    }
+    fetchDrained();
+  }, []);
 
   return (
     <Card className="w-[550px] m-auto">
