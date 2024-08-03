2984d4d765c9d713b42a50eca77539b142c94a27
Dz.dev <darwinbjordan@gmail.com>
2024-08-03 17:17:26 +0800

    progress: run build the app and added server static from hono to implement static files and routes for the app when deployed

--- server/app.ts
+++ server/app.ts
@@ -1,4 +1,5 @@
 import { Hono } from "hono";
+import { serveStatic } from "hono/bun";
 import { logger } from "hono/logger";
 import { liabilitiesRoute } from "./routes/liabilities";
 
@@ -6,10 +7,9 @@ const app = new Hono();
 
 app.use("*", logger());
 
-app.get("/test", (c) => {
-  return c.json({ message: "test" });
-});
-
 app.route("/api/liabilities", liabilitiesRoute);
+
+app.get("*", serveStatic({ root: "./client/dist" }));
+app.get("*", serveStatic({ path: "./client/dist/index.html" }));
 
 export default app;
