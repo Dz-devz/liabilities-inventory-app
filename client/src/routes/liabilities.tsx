16492dbf2af8711bb8af76c44af0a6012593831d
Dz.dev <darwinbjordan@gmail.com>
2024-08-09 14:39:46 +0800

    progress: added shadcn ui in liabilities page and skeleton load

--- client/src/routes/liabilities.tsx
+++ client/src/routes/liabilities.tsx
@@ -1,3 +1,14 @@
+import { Skeleton } from "@/components/ui/skeleton";
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+
 import { api } from "@/lib/api";
 import { useQuery } from "@tanstack/react-query";
 import { createFileRoute } from "@tanstack/react-router";
@@ -24,8 +35,42 @@ function Liabilities() {
   if (error) return "An Error has occured" + error.message;
 
   return (
-    <div className="p-2">
-      <pre>{isPending ? "..." : JSON.stringify(data, null, 2)}</pre>
+    <div className="p-2 max-w-4xl m-auto">
+      <Table>
+        <TableCaption>A list of all Liabilities</TableCaption>
+        <TableHeader>
+          <TableRow>
+            <TableHead className="w-[100px]">Id</TableHead>
+            <TableHead>Title</TableHead>
+            <TableHead>Amount</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          {isPending
+            ? Array(5)
+                .fill(0)
+                .map((_, i) => (
+                  <TableRow key={i}>
+                    <TableCell className="font-medium">
+                      <Skeleton className="h-4" />
+                    </TableCell>
+                    <TableCell>
+                      <Skeleton className="h-4" />
+                    </TableCell>
+                    <TableCell>
+                      <Skeleton className="h-4" />
+                    </TableCell>
+                  </TableRow>
+                ))
+            : data?.liabilities.map((liability) => (
+                <TableRow key={liability.id}>
+                  <TableCell className="font-medium">{liability.id}</TableCell>
+                  <TableCell>{liability.title}</TableCell>
+                  <TableCell>{liability.amount}</TableCell>
+                </TableRow>
+              ))}
+        </TableBody>
+      </Table>
     </div>
   );
 }
