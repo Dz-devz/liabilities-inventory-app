99031448edb3135acaaa6797f818cfecb8d4ae6c
Dz.dev <darwinbjordan@gmail.com>
2024-08-09 14:14:04 +0800

    progress: get all drained json data in liabilities page

--- client/src/routes/liabilities.tsx
+++ client/src/routes/liabilities.tsx
@@ -1,9 +1,31 @@
+import { api } from "@/lib/api";
+import { useQuery } from "@tanstack/react-query";
 import { createFileRoute } from "@tanstack/react-router";
 
 export const Route = createFileRoute("/liabilities")({
   component: Liabilities,
 });
 
+async function getAllDrained() {
+  const res = await api.liabilities.$get();
+  if (!res.ok) {
+    throw new Error("Server error");
+  }
+  const data = await res.json();
+  return data;
+}
+
 function Liabilities() {
-  return <div className="p-2">Show All Liabilities</div>;
+  const { isPending, error, data } = useQuery({
+    queryKey: ["get-all-liabilities"],
+    queryFn: getAllDrained,
+  });
+
+  if (error) return "An Error has occured" + error.message;
+
+  return (
+    <div className="p-2">
+      <pre>{isPending ? "..." : JSON.stringify(data, null, 2)}</pre>
+    </div>
+  );
 }
