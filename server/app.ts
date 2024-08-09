cc23c932d0d367d1a9943bb421072e41a46dcacc
Dz.dev <darwinbjordan@gmail.com>
2024-08-09 22:42:15 +0800

    progress: added auth using kinde and set it up

--- server/app.ts
+++ server/app.ts
@@ -1,13 +1,17 @@
 import { Hono } from "hono";
 import { serveStatic } from "hono/bun";
 import { logger } from "hono/logger";
+import { authRoute } from "./routes/auth";
 import { liabilitiesRoute } from "./routes/liabilities";
 
 const app = new Hono();
 
 app.use("*", logger());
 
-const apiRoutes = app.basePath("/api").route("/liabilities", liabilitiesRoute);
+const apiRoutes = app
+  .basePath("/api")
+  .route("/liabilities", liabilitiesRoute)
+  .route("/", authRoute);
 
 app.get("*", serveStatic({ root: "./client/dist" }));
 app.get("*", serveStatic({ path: "./client/dist/index.html" }));
