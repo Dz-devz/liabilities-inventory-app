456446834518c78ff79b80c8fe866397185e8699
Dz.dev <darwinbjordan@gmail.com>
2024-08-09 15:22:49 +0800

    progress: added shadcn ui for create-liabilities input and label

--- client/src/routes/create-liabilities.tsx
+++ client/src/routes/create-liabilities.tsx
@@ -1,3 +1,6 @@
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
 import { createFileRoute } from "@tanstack/react-router";
 
 export const Route = createFileRoute("/create-liabilities")({
@@ -5,5 +8,18 @@ export const Route = createFileRoute("/create-liabilities")({
 });
 
 function CreateLiabilities() {
-  return <div className="p-2">Create Liabilities</div>;
+  return (
+    <div className="p-2">
+      <h2>Create Liabilities</h2>
+      <form className="max-w-sm m-auto">
+        <Label htmlFor="title">Title</Label>
+        <Input type="text" id="title" placeholder="Title" />
+        <Label htmlFor="amount">Amount</Label>
+        <Input type="number" id="amount" placeholder="Amount" />
+        <Button className="mt-4" type="submit">
+          Create Liabilities
+        </Button>
+      </form>
+    </div>
+  );
 }
