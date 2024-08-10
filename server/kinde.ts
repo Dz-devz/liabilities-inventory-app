1844f743a4e192950c037f17da35360e44dd0810
Dz.dev <darwinbjordan@gmail.com>
2024-08-10 09:34:45 +0800

    progress: created middleware for getting user in kinde

--- server/kinde.ts
+++ server/kinde.ts
@@ -2,11 +2,12 @@ import {
   createKindeServerClient,
   GrantType,
   type SessionManager,
+  type UserType,
 } from "@kinde-oss/kinde-typescript-sdk";
 import type { Context } from "hono";
 import { deleteCookie, getCookie, setCookie } from "hono/cookie";
+import { createMiddleware } from "hono/factory";
 
-// Client for authorization code flow
 export const kindeClient = createKindeServerClient(
   GrantType.AUTHORIZATION_CODE,
   {
@@ -45,4 +46,26 @@ export const sessionManager = (c: Context): SessionManager => ({
       deleteCookie(c, key);
     });
   },
+});
+
+type Env = {
+  Variables: {
+    user: UserType;
+  };
+};
+
+export const getProfile = createMiddleware<Env>(async (c, next) => {
+  try {
+    const seshManager = sessionManager(c);
+    const isAuthenticated = await kindeClient.isAuthenticated(seshManager);
+    if (!isAuthenticated) {
+      return c.json({ error: "Unauthorized" }, 401);
+    }
+    const user = await kindeClient.getUserProfile(seshManager);
+    c.set("user", user);
+    await next();
+  } catch (e) {
+    console.error(e);
+    return c.json({ error: "Unauthorized" }, 401);
+  }
 });
