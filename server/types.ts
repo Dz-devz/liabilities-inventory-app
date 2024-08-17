2fb560de7b5bd9333f09224276fc6e760328cd95
Dz.dev <darwinbjordan@gmail.com>
2024-08-17 15:52:10 +0800

    chore: added message error

--- server/types.ts
+++ server/types.ts
@@ -6,7 +6,11 @@ export const liabilitiesSchema = z.object({
     .string()
     .min(2, { message: "Title must be at least 2 characters" })
     .max(25, { message: "Title must be at most 25" }),
-  amount: z.string().regex(/^\d+(\.\d{1,2})?$/),
+  amount: z
+    .string()
+    .regex(/^\d+(\.\d{1,2})?$/, {
+      message: "Amount must be positive and reasonable.",
+    }),
 });
 
 export const createSchema = liabilitiesSchema.omit({ id: true });
