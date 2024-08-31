f3b1e3467e31da8e42daf0f0ff99b7c7664c470f
Dz.dev <darwinbjordan@gmail.com>
2024-08-31 14:31:48 +0800

    issue: downloading shadncn ui issue

---
+++ client/src/components/footer.tsx
@@ -0,0 +1,11 @@
+function Footer() {
+  return (
+    <footer className=" text-white py-4 bg-secondary">
+      <div className="text-center">
+        <p>&copy; 2024 Dz.dev. All rights reserved.</p>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
