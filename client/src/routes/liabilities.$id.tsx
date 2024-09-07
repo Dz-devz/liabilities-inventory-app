5d44d82ab77e61f27f9645ccd69196ebfe5253e2
Dz.dev <darwinbjordan@gmail.com>
2024-09-07 23:14:58 +0800

    chore: edit chart with total and amount diff

--- client/src/routes/liabilities.$id.tsx
+++ client/src/routes/liabilities.$id.tsx
@@ -1,10 +1,9 @@
 "use client";
-import { getSingleLiabilities } from "@/lib/api";
+import { drainedQuery, getSingleLiabilities } from "@/lib/api";
 import { useQuery } from "@tanstack/react-query";
 import { createFileRoute } from "@tanstack/react-router";
 
 import { TrendingUp } from "lucide-react";
-import * as React from "react";
 import { Label, Pie, PieChart } from "recharts";
 
 import {
@@ -33,15 +32,15 @@ const chartData = [
 ];
 
 const chartConfig = {
-  visitors: {
-    label: "Visitors",
+  amount: {
+    label: "Amount",
   },
   chrome: {
-    label: "Chrome",
+    label: "Total",
     color: "hsl(var(--chart-1))",
   },
-  safari: {
-    label: "Safari",
+  total: {
+    label: "total",
     color: "hsl(var(--chart-2))",
   },
   firefox: {
@@ -69,6 +68,11 @@ function SingleLiabilities() {
     queryFn: () => getSingleLiabilities({ id: id }), // Fetch based on selected liability ID on Route.params
     enabled: !!id,
   });
+  const {
+    isPending: isPendingTotal,
+    error: errorTotal,
+    data: totalDrainedData,
+  } = useQuery(drainedQuery);
 
   if (isLoading) return <div>Loading...</div>;
   if (error) return <div>Error loading liability details</div>;
@@ -78,84 +82,88 @@ function SingleLiabilities() {
   // to ensure getting a single Data
   const liability = data.singleLiabilities[0];
 
+  const liabilityData = [
+    {
+      id: liability.id,
+      title: liability.title,
+      amount: Number(liability.amount),
+      fill: "var(--color-chrome)",
+    },
+    {
+      title: "Total",
+      amount: Number(totalDrainedData?.total),
+      fill: "var(--color-firefox)",
+    },
+  ];
+
   return (
     <div>
       <h2>{liability.title}</h2>
       <p>Amount: {liability.amount}</p>
-      <Component />
-    </div>
-  );
-}
-
-export function Component() {
-  const totalVisitors = React.useMemo(() => {
-    return chartData.reduce((acc, curr) => acc + curr.visitors, 0);
-  }, []);
-
-  return (
-    <Card className="flex flex-col">
-      <CardHeader className="items-center pb-0">
-        <CardTitle>Pie Chart - Donut with Text</CardTitle>
-        <CardDescription>January - June 2024</CardDescription>
-      </CardHeader>
-      <CardContent className="flex-1 pb-0">
-        <ChartContainer
-          config={chartConfig}
-          className="mx-auto aspect-square max-h-[250px]"
-        >
-          <PieChart>
-            <ChartTooltip
-              cursor={false}
-              content={<ChartTooltipContent hideLabel />}
-            />
-            <Pie
-              data={chartData}
-              dataKey="visitors"
-              nameKey="browser"
-              innerRadius={60}
-              strokeWidth={5}
-            >
-              <Label
-                content={({ viewBox }) => {
-                  if (viewBox && "cx" in viewBox && "cy" in viewBox) {
-                    return (
-                      <text
-                        x={viewBox.cx}
-                        y={viewBox.cy}
-                        textAnchor="middle"
-                        dominantBaseline="middle"
-                      >
-                        <tspan
+      <Card className="flex flex-col">
+        <CardHeader className="items-center pb-0">
+          <CardTitle>Pie Chart</CardTitle>
+          <CardDescription>January - June 2024</CardDescription>
+        </CardHeader>
+        <CardContent className="flex-1 pb-0">
+          <ChartContainer
+            config={chartConfig}
+            className="mx-auto aspect-square max-h-[250px]"
+          >
+            <PieChart>
+              <ChartTooltip
+                cursor={false}
+                content={<ChartTooltipContent hideLabel />}
+              />
+              <Pie
+                data={liabilityData}
+                dataKey="amount"
+                nameKey="title"
+                innerRadius={60}
+                strokeWidth={5}
+              >
+                <Label
+                  content={({ viewBox }) => {
+                    if (viewBox && "cx" in viewBox && "cy" in viewBox) {
+                      return (
+                        <text
                           x={viewBox.cx}
                           y={viewBox.cy}
-                          className="fill-foreground text-3xl font-bold"
+                          textAnchor="middle"
+                          dominantBaseline="middle"
                         >
-                          {totalVisitors.toLocaleString()}
-                        </tspan>
-                        <tspan
-                          x={viewBox.cx}
-                          y={(viewBox.cy || 0) + 24}
-                          className="fill-muted-foreground"
-                        >
-                          Visitors
-                        </tspan>
-                      </text>
-                    );
-                  }
-                }}
-              />
-            </Pie>
-          </PieChart>
-        </ChartContainer>
-      </CardContent>
-      <CardFooter className="flex-col gap-2 text-sm">
-        <div className="flex items-center gap-2 font-medium leading-none">
-          Trending up by 5.2% this month <TrendingUp className="h-4 w-4" />
-        </div>
-        <div className="leading-none text-muted-foreground">
-          Showing total visitors for the last 6 months
-        </div>
-      </CardFooter>
-    </Card>
+                          <tspan
+                            x={viewBox.cx}
+                            y={viewBox.cy}
+                            className="fill-foreground text-3xl font-bold"
+                          >
+                            {totalDrainedData?.total}
+                          </tspan>
+                          <tspan
+                            x={viewBox.cx}
+                            y={(viewBox.cy || 0) + 24}
+                            className="fill-muted-foreground"
+                          >
+                            Total
+                          </tspan>
+                        </text>
+                      );
+                    }
+                  }}
+                />
+              </Pie>
+            </PieChart>
+          </ChartContainer>
+        </CardContent>
+        <CardFooter className="flex-col gap-2 text-sm">
+          <div className="flex items-center gap-2 font-medium leading-none">
+            Spend <TrendingUp className="h-4 w-4" />
+          </div>
+          <div className="leading-none text-muted-foreground">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          </div>
+        </CardFooter>
+      </Card>
+    </div>
   );
 }
